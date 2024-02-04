1c64a6b2bdc7fc79b6492d7991dc352402c431fb
Sherry <>
2024-02-04 14:24:08 +0800

    update

--- js/base.js
+++ js/base.js
@@ -1,8 +1,6 @@
 base = function () {
 	//private menbers
 	let imageUrls = ["./images/section/landing/title.png","./images/common/bg.jpg",  "./images/common/bg-2.jpg", "./images/common/bg-3.jpg"];
-	let arrowLeftAniTl = null;
-	let arrowRightAniTl = null;
 
 	//private methods
 	function init() {
@@ -39,12 +37,7 @@ base = function () {
 
 
 		// 重設section定位
-		viewModel.resetSection();
-
-		// 等影片載完再進入頁面
-		// loadVideo().then(()=>{
-		// 	startHandler
-		// });
+		viewModel.resetSection();;
 
 		// 等背景載完再進入頁面
 		loadImagesAndPerformAction(imageUrls, startHandler);
@@ -72,8 +65,6 @@ base = function () {
 			// horizontalHandler();
 
 			// console.log("_section: "+_section)
-
-			$(".floatbutton-discount").show();
 			$(".logo-title").show();
 
 			if(_section === ".landing") {
@@ -94,49 +85,6 @@ base = function () {
 				}, 100);
 			}
 
-			if (_section === ".creators-leeon") {
-				setTimeout(() => {
-					creatorsPage.openAni_leeon();
-				}, 100);
-			}
-
-			if (_section === ".creators-video-huber") {
-				setTimeout(() => {
-					creatorsPage.openAni_video_huber();
-				}, 100);
-			}
-
-			if (_section === ".creators-video-leeon") {
-				setTimeout(() => {
-					creatorsPage.openAni_video_leeon();
-				}, 100);
-			}
-
-			if (_section === ".product-choose") {
-				setTimeout(() => {
-					productPage.openAni_choose();
-				}, 100);
-			}
-			
-			if (_section === ".product-precision") {
-				setTimeout(() => {
-					productPage.openAni_precision();
-				}, 100);
-			}
-
-			if (_section === ".product-inspiron") {
-				setTimeout(() => {
-					productPage.openAni_inspiron();
-				}, 100);
-			}
-			
-			if (_section === ".strongpoint") {
-				$(".floatbutton-discount").hide();
-				setTimeout(() => {
-					strongpointPage.openAni();
-				}, 100);
-			}
-
 		})
 
 		$(".game__resizeArea").on("click", ".goToNinesquare", function() {
@@ -145,40 +93,6 @@ base = function () {
 			$(".modal-explan").modal("hide");
 		});
 
-	}
-
-	// 載入影片後再執行
-	function loadVideo() {
-		return new Promise(resolve => {
-			const videos = document.querySelectorAll('video');
-			let videosNum = videos.length;
-			let loadVideosNum = 0;
-			console.log("videosNum: "+videosNum)
-
-			if(videosNum > 0) {
-				videos.forEach(video => {
-					console.log(video)
-					video.addEventListener('loadedmetadata', function() {
-						console.log('影片元數據已載入完畢！');
-						loadVideosNum++
-						if(loadVideosNum >= videosNum) {
-							resolve("影片元數據已載入完畢！");
-						}
-						console.log("loadVideosNum: "+loadVideosNum)
-					});
-	
-					if (video.readyState >= 1) {
-						console.log('影片已經載入完畢！');
-						return true
-					} else {
-						console.log('影片還在載入中...');
-					}
-				});
-			}else {
-				resolve("無影片！");
-			}
-
-		});
 	}
 
 	function loadImage(src) {
@@ -220,42 +134,6 @@ base = function () {
 		// console.log("getIsFbApp:  "+ResponsivePage.getIsFbApp());
 	}
 
-	// 左右箭頭動態
-	function arrowAni(el) {
-		arrowLeftAni(el);
-		arrowRightAni(el);
-	}
-
-	function arrowLeftAni(el) {
-		arrowLeftAniTl = gsap.timeline({ repeat: -1, repeatDelay: 0.3 });
-		arrowLeftAniTl.to(el + " .selector__arrow-left img:nth-child(3)", { duration: 0.3, opacity: 1 });
-		arrowLeftAniTl.to(el + " .selector__arrow-left img:nth-child(2)", { duration: 0.3, opacity: 1 });
-		arrowLeftAniTl.to(el + " .selector__arrow-left img:nth-child(1)", { duration: 0.3, opacity: 1 });
-		arrowLeftAniTl.to(el + " .selector__arrow-left img:nth-child(3)", { duration: 0.3, opacity: 0.2 }, 0.5);
-		arrowLeftAniTl.to(el + " .selector__arrow-left img:nth-child(2)", { duration: 0.3, opacity: 0.2 }, 0.8);
-		arrowLeftAniTl.to(el + " .selector__arrow-left img:nth-child(1)", { duration: 0.3, opacity: 0.2 }, 1.1);
-	}
-
-	function arrowRightAni(el) {
-		arrowRightAniTl = gsap.timeline({ repeat: -1, repeatDelay: 0.3 });
-		arrowRightAniTl.to(el + " .selector__arrow-right img:nth-child(1)", { duration: 0.3, opacity: 1 });
-		arrowRightAniTl.to(el + " .selector__arrow-right img:nth-child(2)", { duration: 0.3, opacity: 1 });
-		arrowRightAniTl.to(el + " .selector__arrow-right img:nth-child(3)", { duration: 0.3, opacity: 1 });
-		arrowRightAniTl.to(el + " .selector__arrow-right img:nth-child(1)", { duration: 0.3, opacity: 0.2 }, 0.5);
-		arrowRightAniTl.to(el + " .selector__arrow-right img:nth-child(2)", { duration: 0.3, opacity: 0.2 }, 0.8);
-		arrowRightAniTl.to(el + " .selector__arrow-right img:nth-child(3)", { duration: 0.3, opacity: 0.2 }, 1.1);
-	}
-
-	function killArrowAni() {
-		if(arrowLeftAniTl) {
-			arrowLeftAniTl.kill();
-			arrowLeftAniTl = null;
-		}
-		if(arrowRightAniTl) {
-			arrowRightAniTl.kill();
-			arrowRightAniTl = null;
-		}
-	}
 
 	{
 		$(document).ready(function () {
@@ -268,7 +146,6 @@ base = function () {
 		arrowAni: function(el){
 			arrowAni(el)
 		},
-		killArrowAni
 	};
 };
 
